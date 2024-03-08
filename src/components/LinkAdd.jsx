c8e1c3540c65623b899ea8133eab5f22fe579ec3
Dodam0719 <blueoh719@gmail.com>
2024-03-08 16:24:36 +0900

    ♻️ 🎨 refactor: LinkAdd에 css 연결

--- src/components/LinkAdd.jsx
+++ src/components/LinkAdd.jsx
@@ -1,8 +1,15 @@
+import style from "../styles/LinkAdd.module.css";
+
 // FolderHeader LinkAdd 부분
 function LinkAdd() {
   return (
-    <div>
-      <h2>FolderHeader의 LinkAdd입니다.</h2>
+    <div className={style.link_add_container}>
+      <div className={style.link_add}>
+        <input type="text" placeholder="링크를 추가해 보세요." readOnly />
+        <button className={style.link_add_btn}>
+          <span>추가하기</span>
+        </button>
+      </div>
     </div>
   );
 }
