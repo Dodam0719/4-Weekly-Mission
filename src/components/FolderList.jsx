8fbf408935ca8275f9c6ed5930719bcbacaa756e
Dodam0719 <blueoh719@gmail.com>
2024-04-04 20:15:18 +0900

    ♻️ 🎨 💄 refactor: FolderContent에 FolderList, CSS 연결

--- src/components/FolderList.jsx
+++ src/components/FolderList.jsx
@@ -1,14 +1,40 @@
-// // FolderList -> 폴더 목록에 필요한 데이터는 “/api/users/1/folders”를 활용
-// //            -> “전체” 폴더에 필요한 링크들 데이터는 “/api/users/1/links”를 활용
-// //            -> 이외의 폴더에 필요한 링크들 데이터는 “/api/users/1/links?folderId={해당 폴더 ID}”를 활용
-// //            -> 폴더 버튼을 클릭하면 폴더에 해당하는 링크들로 카드를 구성
-// //            -> 폴더에 링크 데이터가 없을 때는 저장된 링크가 없다는 UI
-// function FolderList() {
-//   return (
-//     <div>
-//       <h2>FolderList 폴더탭 입니다.</h2>
-//     </div>
-//   );
-// }
+import { useState } from "react";
+import { useGetData } from "./Hooks/useAsync";
+import style from "../styles/FolderList.module.css";
 
-// export default FolderList;
+// FolderList -> 폴더 목록에 필요한 데이터는 “/api/users/1/folders”를 활용
+//            -> “전체” 폴더에 필요한 링크들 데이터는 “/api/users/1/links”를 활용
+//            -> 이외의 폴더에 필요한 링크들 데이터는 “/api/users/1/links?folderId={해당 폴더 ID}”를 활용
+//            -> 폴더 버튼을 클릭하면 폴더에 해당하는 링크들로 카드를 구성
+//            -> 폴더에 링크 데이터가 없을 때는 저장된 링크가 없다는 UI
+function FolderList({ endpoint }) {
+  const { data, error, loading } = useGetData(endpoint);
+  const [activeTab, setActiveTab] = useState(0);
+
+  if (loading)
+    return <div>폴더 정보를 불러오는 중이예요. 잠시만 기다려주세요.</div>;
+  if (error)
+    return <div>폴더 정보를 조회하지 못했어요! 나중에 다시 시도해주세요.</div>;
+
+  const changeTab = (id) => {
+    setActiveTab(id);
+  };
+
+  return (
+    <div className={style.tabs}>
+      {data &&
+        Array.isArray(data.data) &&
+        data.data.map((folder, index) => (
+          <button
+            key={folder.id}
+            className={`${activeTab === index ? style.active : ""}`}
+            onClick={() => changeTab(index)}
+          >
+            {folder.name}
+          </button>
+        ))}
+    </div>
+  );
+}
+
+export default FolderList;
