91907612cea08adc37dfbd62242f5056fdc048d8
Dodam0719 <blueoh719@gmail.com>
2024-04-04 19:51:46 +0900

    ♻️ 🎨 refactor: FolderContent에 CommonContent 연결

--- src/components/common/CommonContent.jsx
+++ src/components/common/CommonContent.jsx
@@ -1,15 +1,16 @@
+import React from "react";
 import CommonContentSearch from "./CommonContentSearch";
 import CardList from "./CardList";
 import style from "../../styles/CommonContent.module.css";
 
 // SharedContent에는 additionalContent 없음X, FolderHeader에서는 additionalContent 있음O
-function CommonContent(additionalContent) {
+function CommonContent({ children }) {
   // CommonContentSearch -> 링크 검색 부분(기능은 추후 구현)
   // CardList -> 카드 목록 부분(SharedContent와 FolderHeader의 API 주소 다름 주의!)
   return (
     <div className={style.content_container}>
       <CommonContentSearch />
-      {/* {additionalContent && <div>{additionalContent}</div>} */}
+      {children}
       <CardList />
     </div>
   );
--- src/containers/FolderContent.jsx
+++ src/containers/FolderContent.jsx
@@ -1,7 +1,9 @@
-// import FolderList from "../components/FolderList";
-// import AddFolderButton from "../components/AddFolderButton";
+import React from "react";
+import CommonContent from "../components/common/CommonContent";
+import FolderList from "../components/FolderList";
+import AddFolderButton from "../components/AddFolderButton";
+import style from "../styles/SharedContent.module.css";
 // import FolderActions from "../components/FolderActions";
-// import CommonContent from "../components/common/CommonContent";
 
 // // SharedContent FolderContent 겹치는 부분은 공통 컴포넌트인 CommonContent로 관리
 // // FolderContent에서는 CommonContent에 additionalContent 전달 (SharedContent에는 없는 부분)
@@ -29,8 +31,13 @@
 
 function FolderContent() {
   return (
-    <div>
-      <h2>FolderContent입니다.</h2>
+    <div className={style.shared_content}>
+      <CommonContent>
+        <div>
+          <FolderList endpoint="/users/1/folders" />
+          <AddFolderButton />
+        </div>
+      </CommonContent>
     </div>
   );
 }
